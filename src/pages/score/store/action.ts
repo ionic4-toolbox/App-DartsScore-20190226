e893848d87b9341b868950bafefbb6f912f66b62
ferretdayo <ferretdayo@hotmail.co.jp>
2018-05-04 17:55:02 +0900

    不要なActionsを削除，変数名を修正

--- src/pages/score/store/action.ts
+++ src/pages/score/store/action.ts
@@ -1,38 +1,23 @@
 import { Action } from '@ngrx/store'
 import { Score } from '../../../entities/Score'
 
-export const CREATE         = '[Score] Create'
-export const CREATE_SUCCESS = '[Score] Create Success'
-export const CREATE_FAILURE = '[Score] Create Failure'
-export const CHANGE_SCORE_RESULT = '[Score] Change Score Result'
+export const CHANGE_SCORES = '[Score] Change Scores'
+export const CHANGE_RESULT_SCORES = '[Score] Change Result Scores'
 
 /**
- * 作成
+ * スコアテーブルが変わった時
  */
-export class Create implements Action {
-  readonly type = CREATE;
+export class ChangeScores implements Action {
+  readonly type = CHANGE_SCORES;
   constructor(public payload: Score[][]) {}
 }
 
-export class ChangeScoreResult implements Action {
-  readonly type = CHANGE_SCORE_RESULT
+/**
+ * スコアテーブルの結果が変わった時
+ */
+export class ChangeResultScores implements Action {
+  readonly type = CHANGE_RESULT_SCORES
   constructor(public payload: Score[]) {}
 }
 
-/**
- * 作成成功
- */
-export class CreateSuccess implements Action {
-  readonly type = CREATE_SUCCESS;
-  constructor(public payload: Score[][]) {}
-}
-
-/**
- * 作成失敗
- */
-export class CreateFailure implements Action {
-  readonly type = CREATE_FAILURE;
-  constructor(public payload?: any) {}
-}
-
-export type Actions = Create | CreateSuccess | CreateFailure | ChangeScoreResult;
+export type Actions = ChangeScores | ChangeResultScores;
--- src/pages/score/store/reducer.ts
+++ src/pages/score/store/reducer.ts
@@ -6,20 +6,12 @@ import { State, initialState } from './state'
  */
 export function reducer(state = initialState, action: ScoreAction.Actions): State {
   switch (action.type) {
-    case ScoreAction.CREATE: {
+    case ScoreAction.CHANGE_SCORES: {
       // 作成
       return Object.assign({}, state, { loading: true, scores: action.payload });
     }
-    case ScoreAction.CHANGE_SCORE_RESULT: {
+    case ScoreAction.CHANGE_RESULT_SCORES: {
       return Object.assign({}, state, { loading: true, resultScores: action.payload })
-    }
-    case ScoreAction.CREATE_SUCCESS: {
-      // 作成成功したら一覧に追加
-      return Object.assign({}, state, { loading: false, scores: action.payload });
-    }
-    case ScoreAction.CREATE_FAILURE: {
-      // 作成失敗
-      return Object.assign({}, state, { loading: false });
     }
     default: {
       return state;
