054d37f5eac1b4895e2eece8fc1f8167d4e8d908
ferretdayo <ferretdayo@hotmail.co.jp>
2018-05-07 23:11:36 +0900

    add keyboard mock

---
+++ src/components/cricket-keyboard/cricket-keyboard.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+/**
+ * Generated class for the CricketKeyboardComponent component.
+ *
+ * See https://angular.io/api/core/Component for more info on Angular
+ * Components.
+ */
+@Component({
+  selector: 'cricket-keyboard',
+  templateUrl: 'cricket-keyboard.html'
+})
+export class CricketKeyboardComponent {
+
+  constructor() {
+  }
+
+  onTap() {
+    
+  }
+}
